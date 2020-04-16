39db3b4377c9ba722c2389aa9a80dcf39c70fdb5
h82258652 <842053625@qq.com>
2020-04-16 11:15:53 +0800

    commit

---
+++ Demo/Demo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Demo
+{
+    public sealed partial class MainPage 
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FireButton_Click(object sender, RoutedEventArgs e)
+        {
+            var connectedAnimationService = ConnectedAnimationService.GetForCurrentView();
+            var connectedAnimation = connectedAnimationService.PrepareToAnimate("Animation", AnimationSource);
+            connectedAnimation.TryStart(AnimationTarget);
+
+            AnimationSource.Opacity = 0;
+        }
+    }
+}
